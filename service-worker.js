f2d0a29e5b44f0da63265166050330dadb0d476a
misrecuerdos-app <misrecuerdos.app@gmail.com>
2026-07-27 17:44:35 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,6 +18,7 @@ const APP_SHELL = [
   "./assets/images/icons/apple-touch-icon.png"
 ];
 
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
