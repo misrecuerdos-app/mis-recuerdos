15fde5ec517e31d430ef065c8290721ca52e4bbc
misrecuerdos-app <misrecuerdos.app@gmail.com>
2026-07-28 20:09:40 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -81,7 +81,9 @@ function getVideoPlaceholderDataUrl() {
       <polygon points="280,220 280,300 350,260" fill="white"/>
       <text x="300" y="380" text-anchor="middle"
         font-family="Arial, sans-serif" font-size="48" fill="#5a3150">
-        Video Seleccionado, Oprime "Subir" Miniatura no disponible
+        Video Seleccionado, 
+        Miniatura no disponible
+        Oprime "Subir"
       </text>
     </svg>
   `;
